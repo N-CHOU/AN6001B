fcda8dc133318c49b0d7aa1238110f14996a9ec8
Nabodita Choudhury <82321875+N-CHOU@users.noreply.github.com>
2025-11-14 07:45:25 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
+app = Flask(__name__) ## this turns it into the main file I guess?? No, it connects to the naming convention we use to create files and folders
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return (render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
+
