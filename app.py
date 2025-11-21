04b0b227b9e661ca622e6b0e8744d9fa4847bbee
Nabodita Choudhury <82321875+N-CHOU@users.noreply.github.com>
2025-11-21 07:33:23 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import joblib
+
 app = Flask(__name__) ## this turns it into the main file I guess?? No, it connects to the naming convention we use to create files and folders
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -9,6 +11,20 @@ def main():
     q = request.form.get("q")
     print(q)
     return (render_template("main.html"))
+
+@app.route("/dbs", methods=["GET", "POST"])
+def dbs():
+    q = request.form.get("q")
+    print(q)
+    return (render_template("dbs.html"))
+
+@app.route("/dbs_prediction", methods=["GET", "POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    model = joblib.load("dbs.pkl")
+    r = model.predict([[q]])
+    return (render_template("dbs_prediction.html", r=r[0][0]))
+
 
 
 if __name__ == "__main__":
