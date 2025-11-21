e2033029663cc997f4790a0d20b9d132327d1a67
Nabodita Choudhury <82321875+N-CHOU@users.noreply.github.com>
2025-11-21 06:32:03 +0000

    1

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ app = Flask(__name__) ## this turns it into the main file I guess?? No, it conne
 def index():
     return (render_template("index.html"))
 
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    q = request.form.get("q")
+    print(q)
+    return (render_template("main.html"))
+
+
 if __name__ == "__main__":
     app.run()
 
